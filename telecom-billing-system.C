6230c2fc76d8f3764dddfb8e14f38149d70ef14f
pouya1364 <pouya1364@gmail.com>
2020-07-03 10:14:00 +0800

    fix clear screen and enhance some parts

--- telecom-billing-system.C
+++ telecom-billing-system.C
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<ctype.h>
 #include<stdlib.h>
-#include <cstring>
-#include <unistd.h>
+#include<cstring>
+#include<unistd.h>
+#include <cstdlib>
+
 // #include <windows.h> //uncomment it if you are running it in windows
 struct subscriber
 {
@@ -18,23 +20,31 @@ void searchrecords();
 void payment();
 char get;
 
+void clear_screen()
+{
+	#if __LINUX__
+		std::system("clear");
+	#else
+		// Assume POSIX
+		std::system("cls");
+	#endif
+}
+
 int main()
 {	int password;
 	int phonenumber;
 	char choice;
 
-
-
-	system("clear");
-printf("\n\n\n\n\n\n\n\n\n**************************************************************");
-	printf("\n\t\t------TELECOM BILLING MANAGEMENT SYSTEM------");
-	printf("\n\t\t****************************************************************");
-	//sleep(2000);
+	clear_screen();
+	printf("\n\n**************************************************************");
+	printf("\n\t------TELECOM BILLING MANAGEMENT SYSTEM------");
+	printf("\n\n****************************************************************");
+	printf("\n\nPress any key to continue");
 	getchar();
-    system("clear");
+    clear_screen();
 	while (1)
 	{
-		system("clear");
+		clear_screen();
 		printf("\n Enter\n A : for adding new records.\n L : for list of records");
 		printf("\n M : for modifying records.\n P : for payment");
 		printf("\n S : for searching records.");
@@ -56,51 +66,43 @@ printf("\n\n\n\n\n\n\n\n\n******************************************************
 			case 'D':
 				deleterecords();break;
 			case 'E':
-				system("clear");
+				clear_screen();
 				printf("\n\n\t\t\t\tTHANK YOU");
 				printf("\n\n\n\n\n:\n\tFOR USING OUR SERVICE");
-				//sleep(2000);
 				exit(0);
 				break;
 			default:
-				system("clear");
-				printf("Incorrect Input");
-				printf("\nAny key to continue");
-				getchar();
+				clear_screen();
 		}
 	}
 }
 void addrecords()
 {
 	FILE *f;
-	char test;
+	// char c;
 	f=fopen("file.ojs","ab+");
 	if(f==0)
 	{   f=fopen("file.ojs","wb+");
-		system("clear");
+		clear_screen();
 		printf("please wait while we configure your computer");
 		printf("/npress any key to continue");
 		getchar();
 	}
-	while(1)
-	{
-		system("clear");
-		printf("\n Enter phone number:");
-		scanf("%s",s.phonenumber);
-		printf("\n Enter name:");
-		fflush(stdin);
-		scanf("%s",s.name);
-		printf("\n Enter amount:");
-		scanf("%f",&s.amount);
-		fwrite(&s,sizeof(s),1,f);
-		fflush(stdin);
-		system("clear");
-		printf("1 record successfully added");
-		printf("\n Press esc key to exit, any other key to add other record:");
-		test=getchar();
 
-		if(test==27) break;
-	}
+	clear_screen();
+	printf("\n Enter phone number:");
+	scanf("%s",s.phonenumber);
+	printf("\n Enter name:");
+	fflush(stdin);
+	scanf("%s",s.name);
+	printf("\n Enter amount:");
+	scanf("%f",&s.amount);
+	fwrite(&s,sizeof(s),1,f);
+	fflush(stdin);
+	clear_screen();
+	printf("1 record successfully added");
+	printf("\n Press esc key to exit, any other key to add other record:");
+
 	fclose(f);
 }
 void listrecords()
@@ -109,13 +111,13 @@ void listrecords()
 	int i;
 	if((f=fopen("file.ojs","rb"))==NULL)
 		exit(0);
-	system("clear");
+	clear_screen();
 	printf("Phone Number\t\tUser Name\t\t\tAmount\n");
 	for(i=0;i<79;i++)
 		printf("-");
 	while(fread(&s,sizeof(s),1,f)==1)
 	{
-		printf("\n%-10s\t\t%-20s\t\tRs. %.2f /-",s.phonenumber,s.name,s.amount);
+		printf("\n%-10s\t\t%-20s\t\t$. %.2f /-",s.phonenumber,s.name,s.amount);
 	}
 	printf("\n");
 	for(i=0;i<79;i++)
@@ -133,7 +135,7 @@ void deleterecords()
 	exit(0);
 	if((f=fopen("file.ojs","rb"))==NULL)
 	exit(0);
-	system("clear");
+	clear_screen();
 	printf("Enter the phone number to be deleted from the Database");
 	fflush(stdin);
 	scanf("%[^\n]",phonenumber);
@@ -148,7 +150,7 @@ void deleterecords()
 			fwrite(&s,sizeof(s),1,t);
 	}
 	if(i==1)
-	{       system("clear");
+	{       clear_screen();
 		printf("Phone number \"%s\" not found",phonenumber);
 		remove("file.ojs");
 		rename("temp.ojs","file.ojs");
@@ -159,7 +161,7 @@ void deleterecords()
 	}
 	remove("file.ojs");
 	rename("temp.ojs","file.ojs");
-	system("clear");
+	clear_screen();
 	printf("The Number %s Successfully Deleted!!!!",phonenumber);
 	fclose(f);
 	fclose(t);
@@ -174,20 +176,20 @@ void searchrecords()
 	if(f==0)
 		exit(0);
 	fflush(stdin);
-	system("clear");
+	clear_screen();
 	printf("Enter Phone Number to search in our database");
 	scanf("%s", phonenumber);
 	while(fread(&s,sizeof(s),1,f)==1)
 	{
 		if(strcmp(s.phonenumber,phonenumber)==0)
-		{	system("clear");
+		{	clear_screen();
 			printf(" Record Found ");
 			printf("\n\nPhonenumber: %s\nName: %s\nAmount: Rs.%0.2f\n",s.phonenumber,s.name,s.amount);
 			flag=0;
 			break;
 		}
 		else if(flag==1)
-		{	system("clear");
+		{	clear_screen();
 			printf("Requested Phone Number Not found in our database");
 		}
 	}
@@ -201,7 +203,7 @@ void modifyrecords()
 	long int size=sizeof(s);
 	if((f=fopen("file.ojs","rb+"))==NULL)
 		exit(0);
-	system("clear");
+	clear_screen();
 	printf("Enter phone number of the subscriber to modify:");
 	scanf("%[^\n]",phonenumber);
 	fflush(stdin);
@@ -209,7 +211,7 @@ void modifyrecords()
 	{
 		if(strcmp(s.phonenumber,phonenumber)==0)
 		{
-			system("clear");
+			clear_screen();
 			printf("\n Enter phone number:");
 			scanf("%s",s.phonenumber);
 			printf("\n Enter name: ");
@@ -233,7 +235,7 @@ void payment()
 	int i;
 	if((f=fopen("file.ojs","rb+"))==NULL)
 		exit(0);
-	system("clear");
+	clear_screen();
 	printf("Enter phone number of the subscriber for payment");
 	scanf("%[^\n]",phonenumber);
 	fflush(stdin);
@@ -241,7 +243,7 @@ void payment()
 	{
 		if(strcmp(s.phonenumber,phonenumber)==0)
 		{
-			system("clear");
+			clear_screen();
 			printf("\n Phone No.: %s",s.phonenumber);
 			printf("\n Name: %s",s.name);
 			printf("\n Current amount: %f",s.amount);
@@ -257,7 +259,7 @@ void payment()
 			break;
 		}
 	}
-	system("clear");
+	clear_screen();
 	printf("THANK YOU %s FOR YOUR TIMELY PAYMENTS",s.name);
 	getchar();
 	fclose(f);
